f845e61f7d9acd29f76c6ea481626dd0ed9a5702
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2021-03-12 13:16:12 -0300

    Iniciando bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Projeto criado!');
+import dotenv from 'dotenv';
+dotenv.config();
+
+import Discord from 'discord.js';
+
+const app = new Discord.Client();
+app.login(process.env.BOT_TOKEN);
+
+app.on('ready', () => {
+	console.log('Bot online');
+});
